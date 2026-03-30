62f6607433426a8fcb87c3e23f58259a50ad4ccc
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-30 13:53:05 +0000

    Created a list

--- Classroom.cs
+++ Classroom.cs
@@ -3,6 +3,6 @@
 public class Classroom{
 
     public List<string> CreateTeam(string first, string second, string third){
-        return null;
+        return new List<string> { first, second, third };
     }
 }
